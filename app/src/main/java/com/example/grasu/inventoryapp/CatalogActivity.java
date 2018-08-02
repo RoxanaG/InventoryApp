07758b063ca197b3694952d9c3857a83304abd24
Roxana Gabor <roxanagabor91@gmail.com>
2018-08-02 11:26:03 +0300

    mod1

--- app/src/main/java/com/example/grasu/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/example/grasu/inventoryapp/CatalogActivity.java
@@ -23,7 +23,7 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_book);
+        FloatingActionButton fab = findViewById(R.id.add_book);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,15 +52,15 @@ public class CatalogActivity extends AppCompatActivity {
 
 
         Cursor cursor = db.query(
-                BooksContract.BooksEntry.TABLE_NAME,   // The table to query
-                projection,            // The columns to return
-                null,                  // The columns for the WHERE clause
-                null,                  // The values for the WHERE clause
-                null,                  // Don't group the rows
-                null,                  // Don't filter by row groups
-                null);                   // The sort order
+                BooksContract.BooksEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
 
-        TextView displayView = (TextView) findViewById(R.id.text_book);
+        TextView displayView = findViewById(R.id.text_book);
 
         try {
 
@@ -80,17 +80,15 @@ public class CatalogActivity extends AppCompatActivity {
             int supplierColumnIndex = cursor.getColumnIndex(BooksContract.BooksEntry.COLUMN_BOOKS_SUPPLIER);
             int phoneColumnIndex = cursor.getColumnIndex(BooksContract.BooksEntry.COLUMN_BOOKS_PHONE);
 
-            // Iterate through all the returned rows in the cursor
             while (cursor.moveToNext()) {
-                // Use that index to extract the String or Int value of the word
-                // at the current row the cursor is on.
+
                 int currentID = cursor.getInt(idColumnIndex);
                 String currentProduct = cursor.getString(productColumnIndex);
                 Double currentPrice = cursor.getDouble(priceColumnIndex);
                 int currentQuantity = cursor.getInt(quantityColumnIndex);
                 String currentSupplier = cursor.getString(supplierColumnIndex);
                 int currentPhone = cursor.getInt(phoneColumnIndex);
-                // Display the values from each column of the current row in the cursor in the TextView
+
                 displayView.append(("\n" + currentID + " - " +
                         currentProduct + " - " +
                         currentPrice + " - " +
@@ -99,21 +97,14 @@ public class CatalogActivity extends AppCompatActivity {
                         currentPhone));
             }
         } finally {
-            // Always close the cursor when you're done reading from it. This releases all its
-            // resources and makes it invalid.
             cursor.close();
         }
     }
 
-    /**
-     * Helper method to insert hardcoded pet data into the database. For debugging purposes only.
-     */
     private void insertBooks() {
-        // Gets the database in write mode
+
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        // Create a ContentValues object where column names are the keys,
-        // and Toto's pet attributes are the values.
         ContentValues values = new ContentValues();
         values.put(BooksContract.BooksEntry.COLUMN_BOOKS_PRODUCT, "Walks with men");
         values.put(BooksContract.BooksEntry.COLUMN_BOOKS_PRICE, 10.00);
@@ -121,30 +112,27 @@ public class CatalogActivity extends AppCompatActivity {
         values.put(BooksContract.BooksEntry.COLUMN_BOOKS_SUPPLIER, "Amazon");
         values.put(BooksContract.BooksEntry.COLUMN_BOOKS_PHONE, 727213658);
 
-
         long newRowId = db.insert(BooksContract.BooksEntry.TABLE_NAME, null, values);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu options from the res/menu/menu_catalog.xml file.
-        // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_catalog, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // User clicked on a menu option in the app bar overflow menu
+
         switch (item.getItemId()) {
-            // Respond to a click on the "Insert dummy data" menu option
+
             case R.id.insert_dummy_data:
                 insertBooks();
                 displayDatabaseInfo();
                 return true;
-            // Respond to a click on the "Delete all entries" menu option
+
             case R.id.delete_all_entries:
-                // Do nothing for now
+
                 return true;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/grasu/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/grasu/inventoryapp/EditorActivity.java
@@ -22,24 +22,12 @@ import com.example.grasu.inventoryapp.data.BooksDbHelper;
 
 public class EditorActivity extends AppCompatActivity {
 
-    /**
-     * EditText field to enter the pet's name
-     */
     private EditText productName;
 
-    /**
-     * EditText field to enter the pet's breed
-     */
     private EditText price;
 
-    /**
-     * EditText field to enter the pet's weight
-     */
     private EditText quantity;
 
-    /**
-     * EditText field to enter the pet's gender
-     */
     private EditText supplier;
     private EditText supplierPhone;
 
@@ -48,24 +36,16 @@ public class EditorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
 
-        // Find all relevant views that we will need to read user input from
         productName = (EditText) findViewById(R.id.product_name);
         price = (EditText) findViewById(R.id.price);
         quantity = (EditText) findViewById(R.id.quantity);
         supplier = (EditText) findViewById(R.id.supplier_name);
         supplierPhone = (EditText) findViewById(R.id.supplier_phone);
 
-    }
+    };
 
-    ;
-
-
-    /**
-     * Get user input from editor and save new pet into database.
-     */
     private void insertBooks() {
-        // Read from input fields
-        // Use trim to eliminate leading or trailing white space
+
         String productString = productName.getText().toString().trim();
         String priceString = price.getText().toString().trim();
         int price = Integer.parseInt(priceString);
@@ -75,60 +55,68 @@ public class EditorActivity extends AppCompatActivity {
         String phoneString = supplierPhone.getText().toString().trim();
         int phone = Integer.parseInt(phoneString);
 
-        // Create database helper
         BooksDbHelper dbHelper = new BooksDbHelper(this);
 
-        // Gets the database in write mode
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        // Create a ContentValues object where column names are the keys,
-        // and pet attributes from the editor are the values.
-        ContentValues values = new ContentValues();
+              ContentValues values = new ContentValues();
         values.put(BooksContract.BooksEntry.COLUMN_BOOKS_PRODUCT, productString);
         values.put(BooksContract.BooksEntry.COLUMN_BOOKS_PRICE, priceString);
         values.put(BooksContract.BooksEntry.COLUMN_BOOKS_QUANTITY, quantityString);
         values.put(BooksContract.BooksEntry.COLUMN_BOOKS_SUPPLIER, supplierString);
         values.put(BooksContract.BooksEntry.COLUMN_BOOKS_PHONE, phoneString);
 
-        // Insert a new row for pet in the database, returning the ID of that new row.
         long newRowId = db.insert(BooksContract.BooksEntry.TABLE_NAME, null, values);
-
-        // Show a toast message depending on whether or not the insertion was successful
-        if (newRowId == -1) {
-            // If the row ID is -1, then there was an error with insertion.
-            Toast.makeText(this, "Error with saving book", Toast.LENGTH_SHORT).show();
+               if (newRowId == -1) {
+             Toast.makeText(this, "Error with saving book", Toast.LENGTH_SHORT).show();
         } else {
-            // Otherwise, the insertion was successful and we can display a toast with the row ID.
             Toast.makeText(this, "Book saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu options from the res/menu/menu_editor.xml file.
-        // This adds menu items to the app bar.
+
         getMenuInflater().inflate(R.menu.menu_editor, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // User clicked on a menu option in the app bar overflow menu
+
         switch (item.getItemId()) {
-            // Respond to a click on the "Save" menu option
+
             case R.id.save:
-                          // Save pet to database
-                insertBooks();
-                // Exit activity
-                finish();
+                if (productName.getText().toString().length() == 0 && price.getText().toString().length() == 0 && quantity.getText().toString().length() == 0 && supplierPhone.getText().toString().length() == 0) {
+                    Toast.makeText(this, getString(R.string.nothing_saved), Toast.LENGTH_LONG).show();
+                }else{
+                    if (productName.getText().toString().length() == 0){
+                        Toast.makeText(this, getString(R.string.no_product_name), Toast.LENGTH_LONG).show();
+                    }else {
+                        if (price.getText().toString().length() == 0){
+                            Toast.makeText(this, getString(R.string.no_price), Toast.LENGTH_LONG).show();
+                        }else{
+                            if (quantity.getText().toString().length() == 0){
+                                Toast.makeText(this, getString(R.string.no_quantity), Toast.LENGTH_LONG).show();
+                            }else {
+                                if (supplierPhone.getText().toString().length() == 0) {
+                                    Toast.makeText(this, getString(R.string.no_phone), Toast.LENGTH_LONG).show();
+                                }else{
+                                    insertBooks();
+
+                                    finish();
+                                }
+                            }
+                        }
+                    }}
+
                 return true;
-            // Respond to a click on the "Delete" menu option
+
             case R.id.delete:
 
                 return true;
-            // Respond to a click on the "Up" arrow button in the app bar
+
             case android.R.id.home:
-                // Navigate back to parent activity (CatalogActivity)
+
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
         }
